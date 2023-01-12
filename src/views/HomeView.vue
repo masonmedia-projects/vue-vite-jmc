c778601f5eadc9f979b6a4caed520983d3e03d58
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2023-01-11 19:57:08 -0500

    Update AOS animation attrs, commented cyprus http hoping to fix unsecure flag netlify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,7 +18,7 @@ onMounted(() => {
     offset: 0, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 600, // values from 0 to 3000, with step 50ms
-    easing: 'ease-in-out-cubic', // default easing for AOS animations
+    easing: 'ease-in-out-sine', // default easing for AOS animations
     mirror: true, // whether elements should animate out while scrolling past them
   })
 })
@@ -100,13 +100,6 @@ onMounted(() => {
 
 
           <div class="row min-vh-100" id="mission">
-            <!-- <div class="col-sm-12">
-              <form action="https://lb.benchmarkemail.com//code/lbformnew.js?mFcQnoBFKMQ7RNuiGgq%252F%252BFcgGr0xQgnh4fU4OkXQl8t%252F5wfXxPKeqw%253D%253D">
-              <input type="text" placeholder="test">
-              <input type="email" placeholder="temail">
-              <button value="submit">Submit</button>
-              </form>
-            </div> -->
             <div class="col-lg-8 offset-lg-2 min-vh-100 p-5 center-center">
               <h5 data-aos="fade-up" class="up text-uppercase font-weight-bold" v-html="content.mission.microtitle"></h5>
               <h2 data-aos="fade-up" data-aos-delay="200" class="section-title" v-html="content.mission.title"></h2>
@@ -147,28 +140,10 @@ onMounted(() => {
       v-for="(item, index) in content.services" :key="index">
       <div v-if="index % 2 === 0" class="w-100 d-flex flex-wrap flex-md-nowwrap">
         <div class="col-lg-6 min-50 align-left-center my-auto p-5 m-0 order-2 order-lg-1">
-          <h6 class="up text-uppercase small" v-html="item.microtitle"></h6>
-          <h2 class="up section-title" v-html="item.title"></h2>
-          <div class="up mb-4" v-html="item.text"></div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down" width="60" height="60" viewBox="0 0 24 24" stroke-width=".5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-              <line x1="12" y1="5" x2="12" y2="19" />
-              <line x1="18" y1="13" x2="12" y2="19" />
-              <line x1="6" y1="13" x2="12" y2="19" />
-            </svg>
-        </div>
-        <div class="col-lg-6 min-vh-50 p-0 order-1 order-lg-12">
-          <img width="1000" height="600" :src="`https://content.jenmasonconsulting.ca/cockpit/storage/uploads/${item.img.path}`" class="w-100 h-100 object-fit" alt="" />
-        </div>
-      </div>
-      <div v-else class="w-100 d-flex flex-wrap flex-md-nowwrap">
-        <div class="col-lg-6 min-vh-50 p-0">
-          <img width="1000" height="600" :src="`https://content.jenmasonconsulting.ca/cockpit/storage/uploads/${item.img.path}`" class="w-100 h-100 object-fit" alt="" />
-        </div>
-        <div class="col-lg-6 min-50 align-left-center my-auto p-5 m-0">
-          <h6 class="up text-uppercase small" v-html="item.microtitle"></h6>
-          <h2 class="up section-title" v-html="item.title"></h2>
-          <div class="up mb-4" v-html="item.text"></div>
+          <h6 data-aos="fade-up" class="text-uppercase small" v-html="item.microtitle"></h6>
+          <h2 data-aos="fade-up" data-aos-delay="200" class="up section-title" v-html="item.title"></h2>
+          <div data-aos="fade-up" data-aos-delay="400" class="up mb-4" v-html="item.text"></div>
+          <div data-aos="fade-up" data-aos-delay="600">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down" width="60" height="60" viewBox="0 0 24 24" stroke-width=".5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
               <line x1="12" y1="5" x2="12" y2="19" />
@@ -177,16 +152,38 @@ onMounted(() => {
             </svg>
           </div>
         </div>
+        <div class="col-lg-6 min-vh-50 p-0 order-1 order-lg-12">
+          <img data-aos="fade" width="1000" height="600" :src="`https://content.jenmasonconsulting.ca/cockpit/storage/uploads/${item.img.path}`" class="w-100 h-100 object-fit" alt="" />
+        </div>
+      </div>
+      <div v-else class="w-100 d-flex flex-wrap flex-md-nowwrap">
+        <div class="col-lg-6 min-vh-50 p-0">
+          <img data-aos="fade" width="1000" height="600" :src="`https://content.jenmasonconsulting.ca/cockpit/storage/uploads/${item.img.path}`" class="w-100 h-100 object-fit" alt="" />
+        </div>
+        <div class="col-lg-6 min-50 align-left-center my-auto p-5 m-0">
+          <h6 data-aos="fade-up" class="text-uppercase small" v-html="item.microtitle"></h6>
+          <h2 data-aos="fade-up" data-aos-delay="200" class="section-title" v-html="item.title"></h2>
+          <div data-aos="fade-up" data-aos-delay="400" class="mb-4" v-html="item.text"></div>
+          <div class="" data-aos="fade-up" data-aos-delay="600">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down" width="60" height="60" viewBox="0 0 24 24" stroke-width=".5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <line x1="12" y1="5" x2="12" y2="19" />
+              <line x1="18" y1="13" x2="12" y2="19" />
+              <line x1="6" y1="13" x2="12" y2="19" />
+            </svg>
+          </div>
+          </div>
+        </div>
       </div>
 
       <!-- about -->
 
       <div id="about" class="row min-vh-100 bg-linen">
         <div class="col-lg-8 offset-lg-2 min-vh-100 p-5 center-center">
-          <h5 class="up text-uppercase font-weight-bold" v-html="content.about.microtitle"></h5>
-          <h2 class="up section-title" v-html="content.about.title"></h2>
-          <div class="up" v-html="content.about.text"></div>
-            <a href="#contact">
+          <h5 data-aos="fade-up" class="text-uppercase font-weight-bold" v-html="content.about.microtitle"></h5>
+          <h2 data-aos="fade-up" data-aos-delay="200" class="up section-title" v-html="content.about.title"></h2>
+          <div data-aos="fade-up" data-aos-delay="400" class="up" v-html="content.about.text"></div>
+            <a href="#contact" data-aos="fade-up" data-aos-delay="600">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down" width="60" height="60" viewBox="0 0 24 24" stroke-width=".5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
               <line x1="12" y1="5" x2="12" y2="19" />
@@ -196,7 +193,8 @@ onMounted(() => {
             </a>
           </div>
           <div class="col-lg-12 p-0 center-center">
-            <img :src="`https://content.jenmasonconsulting.ca/cockpit/storage/uploads/${content.about.img.path}`"
+            <img data-aos="fade"
+              :src="`https://content.jenmasonconsulting.ca/cockpit/storage/uploads/${content.about.img.path}`"
               width="1000"
               height="600"
               class="img-full min-vh-50"
@@ -208,10 +206,10 @@ onMounted(() => {
 
         <div id="contact" class="row bg-dark text-light">
           <div class="col-lg-8 offset-lg-2 p-5 min-vh-75 center-center">
-            <h5 class="up text-uppercase font-weight-bold" v-html="content.contact.microtitle"></h5>
-            <h1 class="section-title" v-html="content.contact.title"></h1>
-            <p class="mb-4" v-html="content.contact.text"></p>
-            <div class="d-flex">
+            <h5 data-aos="fade-up" class="text-uppercase font-weight-bold" v-html="content.contact.microtitle"></h5>
+            <h1 data-aos="fade-up" data-aos-delay="200" class="section-title" v-html="content.contact.title"></h1>
+            <p data-aos="fade-up" data-aos-delay="400" class="mb-4" v-html="content.contact.text"></p>
+            <div data-aos="fade-up" data-aos-delay="600" class="d-flex">
                 <a :href="content.contactbtnLink">
                 <button role="button" class="btn btn-info me-2" v-html="content.contact.btn"></button>
                 </a>
@@ -220,31 +218,33 @@ onMounted(() => {
               </div>
             </div>
           <div class="col-sm-12 p-0 position-relative">
-            <img width="1000" height="500" class="w-100 object-fit" 
+            <img 
+            data-aos="fade"
+            width="1000" height="500" class="w-100 object-fit" 
             :src="`https://content.jenmasonconsulting.ca/cockpit/storage/uploads/${content.contact.img.path}`" alt="A group jumping on a beach at sunset signifying teamwork, joy, and cameraderie.">
           </div>
         </div>
 
         <!-- Benchmark newsletter signup Modal -->
-    <div class="modal fade" id="signup2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">JMC Newsletter Signup</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body p-2">
-            <iframe style="width:100%; min-height:50vh;" allowfullscreen src="https://lb.benchmarkemail.com//listbuilder/signupnew?IkfHTmyPVq%252BnuC4b%252BprMCP5pwVnAjsSIWFkuV4uvWyDtO5iNRn8gS049TyW7spdJ"></iframe>
+        <div class="modal fade" id="signup2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">JMC Newsletter Signup</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body p-2">
+                <iframe style="width:100%; min-height:50vh;" allowfullscreen src="https://lb.benchmarkemail.com//listbuilder/signupnew?IkfHTmyPVq%252BnuC4b%252BprMCP5pwVnAjsSIWFkuV4uvWyDtO5iNRn8gS049TyW7spdJ"></iframe>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
 
         <!-- footer -->
 
         <footer>
-          <div class="row p-0 m-0">
-            <div class="col-lg-12 p-5 bg-dark center-center">
+          <div class="row p-0 m-0 bg-dark">
+            <div class="col-lg-12 p-5 center-center" data-aos="fade">
               <button type="button" role="button" class="btn btn-outline-light" v-html="content.footer" disabled></button>
             </div>
           </div>
