6fd11077ead79f30478dcdd17608d40529aef435
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2023-01-13 15:21:04 -0500

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-// module.exports = {
-//     publicPath: process.env.NODE_ENV === "production" ? "/vue-vite-jmc/" : "/",
-//   };
+module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? "/vue-vite-jmc/" : "/",
+  };
